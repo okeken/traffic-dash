3840fcf06daff64c7d26e2ea07df6d9499d0fbb2
okeken <okekehinde@gmail.com>
2023-05-31 20:40:58 +0100

    chores:refactor

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,19 +3,11 @@ import "./globals.css";
 import { Analytics } from "@vercel/analytics/react";
 import Nav from "./nav";
 import { Suspense } from "react";
+import Loader from "./loader";
 
 export const metadata = {
-  title: "Next.js 13 + NextAuth + Tailwind CSS",
-  description:
-    "A user admin dashboard configured with Next.js, NextAuth, Tailwind CSS, TypeScript, ESLint, and Prettier.",
-};
-
-const Loader = () => {
-  return (
-    <div className="flex items-center justify-center h-90vh">
-      <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-b-4 border-gray-900"></div>
-    </div>
-  );
+  title: "Organic Traffic Tracker",
+  description: "A Organic Traffic Tracker dashboard For Sales Analysis",
 };
 
 export default async function RootLayout({
--- services/dashboard.ts
+++ services/dashboard.ts
@@ -1,16 +1,15 @@
 import prisma from "../lib/prisma";
-export const fetchDashboard = async (): Promise<[][] | string[][]> => {
-  const data =
-    ((await prisma.traffic.findMany({
-      include: {
-        pages: true,
-      },
-    })) as []) ?? [];
+export const fetchDashboard = async (): Promise<[[], string]> => {
+  const data = (await prisma.traffic.findMany({
+    include: {
+      pages: true,
+    },
+  })) as [];
 
   try {
-    return [data, []];
+    return [data, ""];
   } catch (e) {
     console.log(e, "Erro Fe");
-    return [[], ["Error Fetching  Data" + e]];
+    return [[], "Error Fetching  Data"];
   }
 };
